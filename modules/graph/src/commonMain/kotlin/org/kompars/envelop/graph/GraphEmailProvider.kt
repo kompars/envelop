f4dac1b8d4f6fc2d1de3f9f58a269f3b8bcdc172
morki <morki@morki.cz>
2025-05-20 14:44:51 +0200

    WIP

--- modules/graph/src/commonMain/kotlin/org/kompars/envelop/graph/GraphEmailProvider.kt
+++ modules/graph/src/commonMain/kotlin/org/kompars/envelop/graph/GraphEmailProvider.kt
@@ -2,15 +2,21 @@ package org.kompars.envelop.graph
 
 import org.kompars.envelop.*
 import org.kompars.envelop.blob.*
+import org.kompars.envelop.converter.*
 
 public class GraphEmailProvider internal constructor(
     private val sender: GraphEmailSender,
     private val receiver: GraphEmailReceiver,
-) : EmailProvider, EmailSender by sender, EmailReceiver by receiver
+) : EmailProvider, EmailSender by sender, EmailReceiver by receiver {
+    public suspend fun receive() {
+        receiver.receive()
+    }
+}
 
 public fun GraphEmailProvider(graphApi: GraphApi, blobStorage: BlobStorage = InMemoryBlobStorage): GraphEmailProvider {
-    val sender = GraphEmailSender(graphApi, blobStorage)
-    val receiver = GraphEmailReceiver(graphApi, blobStorage)
+    val converter = EmailConverter(blobStorage)
+    val sender = GraphEmailSender(graphApi, converter)
+    val receiver = GraphEmailReceiver(graphApi, converter)
 
     return GraphEmailProvider(sender, receiver)
 }
